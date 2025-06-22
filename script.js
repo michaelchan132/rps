cc74e0d5f6d577cee30a2d83c6bff29e7c88e403
Michael Chan <110430837+michaelchan132@users.noreply.github.com>
2025-06-21 20:18:13 -0500

    display results using DOM methods

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let buttons = document.querySelector(".buttons");
+let score = document.querySelector("#score");
+let results = document.querySelector("#results");
+let choices = document.querySelector("#choices");
 buttons.addEventListener("click", (event) => {
     let target = event.target;
     switch(target.id){
@@ -46,23 +49,19 @@ let computerScore = 0;
 
 function playRound(playerChoice){
     const computerChoice = getComputerChoice();
+    choices.textContent = `You chose ${playerChoice} | The Computer chose ${computerChoice}`;
     if (playerChoice == computerChoice) {
-        console.log("It's a tie!")        
+        results.textContent =  "It's a tie";
     } else if ((playerChoice == "rock" && computerChoice == "scissors") ||
                 (playerChoice == "paper" && computerChoice == "rock") ||
                 (playerChoice == "scissors" && computerChoice == "paper")
 
     ){
+        results.textContent = "You win";
         ++playerScore
     } else {
-    computerScore++;
-}
-}
-
-if (playerScore > computerScore) {
-    console.log("You win the game");
-} else if (playerScore === computerScore) {
-    console.log("It's a tie");
-} else {
-    console.log("You lose the game");
+        results.textContent = "You lose";
+        computerScore++;
+    }
+    score.textContent = `Your Score is ${playerScore} | The Computers Score is ${computerScore}`;
 }
